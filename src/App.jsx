1b98119c8c11d1dbe981267f7068300de21f0c33
lejames <okejames55@gmail.com>
2023-12-03 18:48:23 +0100

    Add geojson data of sample historical points

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { createContext, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import "leaflet/dist/leaflet.css";
 
 import Map from './components/map'
 import UserLocationMarker from './components/mapComponents/UserLocationMarker';
 import UserLocationAgent from './components/mapComponents/UserLocationAgent';
+import HistoricalData from './components/mapComponents/HistoricalData';
 
 const UserLocationContext = createContext({
   userLocationState: null,
@@ -14,8 +13,6 @@ const UserLocationContext = createContext({
 })
 
 function App() {
-  const [count, setCount] = useState(0)
-
   // {lat: 51.9730, lon: 7.6134} 
   const [ userLocationState, setUserLocationState ] = useState(null)
 
@@ -24,7 +21,8 @@ function App() {
     <UserLocationContext.Provider value={{userLocationState, setUserLocationState}}>
       <div className="h-full w-full">
         <Map>
-          <UserLocationAgent />  
+          <UserLocationAgent />
+          <HistoricalData />
         </Map>
       </div>
     </UserLocationContext.Provider>
