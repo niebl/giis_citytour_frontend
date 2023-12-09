8938cebb5a6b3bce0976d91cbb665d3196f9858c
niebl <caro.niebl@uni-muenster.de>
2023-12-09 16:57:42 +0100

    implement mapViewState and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,42 @@ import UserLocationAgent from './components/mapComponents/UserLocationAgent';
 import UserLocationMarker from './components/mapComponents/UserLocationMarker';
 import HistoricalData from './components/mapComponents/HistoricalData';
 
+import { useRecoilValue, useSetRecoilState  } from "recoil";
+import { mapViewState } from "./atoms";
+import { useEffect } from 'react';
+
 
 function App() {
+  const mapView = useRecoilValue(mapViewState);
+  const setMapView = useSetRecoilState(mapViewState);
+
+  function setViewCruising(state){
+    setMapView('cruising')
+  }
+  function setViewStory(state){
+    setMapView('story')
+  }
+
   return (
     <>
       <div className="h-full w-full">
+        {
+        // the following buttons elements should go into the navigation components
+        // could be replaced with a toggle-button too
+        }
+        <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
+          onClick={setViewCruising}
+          >
+          cruising mode 
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
+          onClick={setViewStory}
+          >
+          story mode
+        </button>
+        <h1>{mapView} mode</h1>
         <Map>
           <UserLocationAgent />
           <UserLocationMarker />
--- src/atoms.jsx
+++ src/atoms.jsx
@@ -6,7 +6,14 @@ import {
     useRecoilValue,
 } from 'recoil';
 
+
+
 export const userLocationState = atom({
   key: 'userLocationState', // unique ID (with respect to other atoms/selectors)
   default: [null, null], // default value (aka initial value)
 });
+
+export const mapViewState = atom({
+  key: 'mapViewState',
+  default: 'cruising' // 'cruising' | 'story' | 'debug'
+})
