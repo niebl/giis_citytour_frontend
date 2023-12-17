5f73524809a7ccf25d384216028c0944abd35fc7
niebl <caro.niebl@uni-muenster.de>
2023-12-17 18:20:06 +0100

    fix site markers not updating properly

--- src/components/mapComponents/views/StoryView.jsx
+++ src/components/mapComponents/views/StoryView.jsx
@@ -1,4 +1,5 @@
-import { FeatureGroup, GeoJSON, Pane } from 'react-leaflet';
+/* eslint-disable react/prop-types */
+import { FeatureGroup, GeoJSON, Marker, Pane, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import { TemplateGeoJSON as tourData } from "../HistoricalData";
 import { useRecoilValue, useSetRecoilState  } from "recoil";
@@ -11,6 +12,28 @@ import {
     gameWaypointProgressState as waypointProgessState 
 } from '../../../atoms';
 
+const buildingIcon = L.icon({
+    iconUrl: icon,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -32],
+})
+
+const MarkerVisited = (props)=>{
+    const feature = props.feature
+    console.log(f)
+    const key = props.key || ""
+    const { name, short_desc, story_desc } = feature.properties
+    const coords = feature.geometry.coordinates
+    return (
+        <Marker icon={buildingIcon} position={[coords[1], coords[0]]} key={key}>
+            <Popup>
+                <b>${name}</b><br />${short_desc}
+            </Popup>
+        </Marker>
+    )
+}
+
 const StoryView = () => {
     //TODO: implement a system that updates player progress
     const waypointProgress = useRecoilValue(waypointProgessState);
@@ -21,12 +44,7 @@ const StoryView = () => {
 
     const waypoints = []
     const waypointsInactive = []
-    const buildingIcon = L.icon({
-        iconUrl: icon,
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-        popupAnchor: [0, -32],
-    })
+
     const waypointMarker = (feature, latlng) => {
         const { name, short_desc, story_desc } = feature.properties
         const marker = L.marker(latlng, { icon: buildingIcon })
@@ -50,13 +68,12 @@ const StoryView = () => {
         <>
         <Pane name="waypoints" style={{ zIndex: 200 }}>
             <FeatureGroup>
-            {tourData.features.map((waypoint) => {
+            {tourData.features.map((waypoint, index) => {
                 if(waypoint.properties.site_index <= waypointProgress){
-                    console.log("one over")
                     return (
                         <GeoJSON
                             data={waypoint}
-                            key={'waypoint'}
+                            key={"wp_"+index}
                             pointToLayer={waypointMarker}
                             //onEachFeature={}
                             //style={}
@@ -70,13 +87,12 @@ const StoryView = () => {
 
         <Pane name="waypointsInactive" style={{ zIndex: 100 }}>
         <FeatureGroup>
-            {tourData.features.map((waypoint) => {
-                if(waypoint.properties.site_index > waypointProgress){
-                    console.log("one under")
+            {tourData.features.map((waypoint, index) => {
+                if(waypoint.properties.site_index > waypointProgress){                    
                     return (               
                         <GeoJSON
-                            data={waypoint}
-                            key={'waypoint'}
+                            data={waypoint  }
+                            key={"wpI_"+index}
                             pointToLayer={waypointMarkerInactive}
                             //onEachFeature={}
                             //style={}
