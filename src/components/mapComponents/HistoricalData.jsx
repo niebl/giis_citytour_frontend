2d1c6a4fb3fdb1aa1ad39c59b4cac77a66a175a8
lejames <okejames55@gmail.com>
2024-01-14 18:37:36 +0100

    Display marker properties on the sidebar

--- src/components/mapComponents/HistoricalData.jsx
+++ src/components/mapComponents/HistoricalData.jsx
@@ -22,6 +22,17 @@ const HistoricalData = ({ setSelectedFeature }) => {
         popupAnchor: [0, -32],
     })
 
+    const showMoreInfo = (featureInfo) => {
+      console.log(featureInfo)
+      setSelectedFeature(featureInfo)
+    }
+
+    // const onMarkerClick = (e) => {
+    //   console.log(e.target)
+    //   const featureProperties = e.target.feature.properties;
+    //   setSelectedFeature(featureProperties)
+    // }; 
+
     useEffect(() => {
       const markers = L.markerClusterGroup({
         disableClusteringAtZoom: 21
@@ -31,13 +42,17 @@ const HistoricalData = ({ setSelectedFeature }) => {
         const marker = L.marker([
           feature.geometry.coordinates[1],
           feature.geometry.coordinates[0],
-        ], { icon: customIcon });
+        ], { icon: customIcon })
+
+        marker.on('click', () => {
+          setSelectedFeature(feature.properties)
+        })
 
         const popupContent = `
             <div class="flex flex-col">
                 <b>${feature.properties.name}</b><br />
                 ${feature.properties.short_desc} <br />
-                <button class="bg-blue-500 text-white py-1 px-2 rounded mt-2 self-end" onclick="showMoreInfo(${JSON.stringify(feature.properties)})">More Info</button>
+                <button class="bg-blue-500 text-white py-1 px-2 rounded mt-2 self-end" onclick="showMoreInfo('${feature.properties}')">More Info</button>
             </div>
         `;
 
@@ -50,30 +65,6 @@ const HistoricalData = ({ setSelectedFeature }) => {
       map.addLayer(markers)
 
     }, [])
-
-    // const onMarkerClick = (e) => {
-    //     const featureProperties = e.target.feature.properties;
-    //     setSelectedFeature(featureProperties)
-    //   }; 
-
-    // const showMoreInfo = (featureInfo) => {
-    //     console.log(featureInfo)
-    // }
-
-    // const createMarker = (feature, latlng) => {
-    //     const { name, short_desc } = feature.properties
-    //     const marker = L.marker(latlng, { icon: customIcon }).on('click', onMarkerClick);
-    //     const popupContent = `
-    //         <div class="flex flex-col">
-    //             <b>${name}</b><br />
-    //             ${short_desc} <br />
-    //             <button class="bg-blue-500 text-white py-1 px-2 rounded mt-2 self-end" onclick="showMoreInfo('${feature.properties}')">More Info</button>
-    //         </div>
-    //     `
-    //     marker.bindPopup(popupContent)
-
-    //     return marker
-    // }
 
   return null;
 }
