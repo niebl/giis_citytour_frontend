7dcc0eac74ffc15d976edd36de4ded5abec5c65c
lejames <okejames55@gmail.com>
2024-01-19 13:15:39 +0100

    Cluster the markers on exploration mode

--- src/components/mapComponents/HistoricalData.jsx
+++ src/components/mapComponents/HistoricalData.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react"
-import { GeoJSON } from "react-leaflet"
+import { useEffect } from "react"
+import { TileLayer, Marker, Popup, GeoJSON, useMap } from "react-leaflet"
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster'
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import { useRecoilValue } from "recoil";
 
 import icon from '../../assets/historyIcon3.svg'
-import templateData from './templateData.json'
 import useExternalData from "./useExternalData";
 import { selectedStoryState } from "../../atoms";
 
 //const data = templateData
 
 const HistoricalData = ({ setSelectedFeature }) => {
+    const map = useMap()
     const story_id = useRecoilValue(selectedStoryState)
     const backendData = useExternalData(story_id)
-
-    if (backendData == undefined){
-      return <></>
-    }
 
     const customIcon = L.icon({
         iconUrl: icon,
@@ -26,33 +25,50 @@ const HistoricalData = ({ setSelectedFeature }) => {
         popupAnchor: [0, -32],
     })
 
-    const onMarkerClick = (e) => {
-        const featureProperties = e.target.feature.properties;
-        setSelectedFeature(featureProperties)
-      }; 
-
     const showMoreInfo = (featureInfo) => {
-        console.log(featureInfo)
+      console.log(featureInfo)
+      setSelectedFeature(featureInfo)
     }
 
-    const createMarker = (feature, latlng) => {
-        const { name, short_desc } = feature.properties
-        const marker = L.marker(latlng, { icon: customIcon }).on('click', onMarkerClick);
+    useEffect(() => {
+      const markers = L.markerClusterGroup({
+        disableClusteringAtZoom: 21
+      })
+
+      backendData?.features.forEach((feature, index) => {
+        const marker = L.marker([
+          feature.geometry.coordinates[1],
+          feature.geometry.coordinates[0],
+        ], { icon: customIcon })
+
+        marker.on('click', () => {
+          setSelectedFeature(feature.properties)
+        })
+
         const popupContent = `
             <div class="flex flex-col">
-                <b>${name}</b><br />
-                ${short_desc} <br />
+                <b>${feature.properties.name}</b><br />
+                ${feature.properties.short_desc} <br />
                 <button class="bg-blue-500 text-white py-1 px-2 rounded mt-2 self-end" onclick="showMoreInfo('${feature.properties}')">More Info</button>
             </div>
-        `
-        marker.bindPopup(popupContent)
+        `;
 
-        return marker
-    }
+        marker.bindPopup(popupContent);
 
-  return (
-    <GeoJSON data={backendData} pointToLayer={createMarker} key={"sites"+Date.now()}/>
-  )
+        markers.addLayer(marker);
+
+      })
+
+      map.addLayer(markers)
+
+      return () => {
+        // remove markers from map when user switches to story mode
+        markers.remove()
+      }
+
+    }, [backendData, map])
+
+  return null;
 }
 
 export default HistoricalData
